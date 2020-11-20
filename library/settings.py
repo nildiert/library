89dbd3e808035167ebecf5d32a9befcb7e008d88
nildiert <niljordan23@gmail.com>
2020-11-20 18:52:53 -0500

    [FEATURE] Added new app password in order to use the email

--- library/settings.py
+++ library/settings.py
@@ -149,5 +149,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'niljordan23@gmail.com'
-EMAIL_HOST_PASSWORD = 'tgmkmutgcknbsrlp'
+EMAIL_HOST_PASSWORD = 'tgphnyuunfdylatj'
 EMAIL_USE_TLS = True
