c0c852b54d823f620df6731a389fa2079ef183a7
nildiert <niljordan23@gmail.com>
2020-11-20 17:16:52 -0500

    [UPDATE] Updated the routes to import the models and the serializers in the tests

--- library/api/tests.py
+++ library/api/tests.py
@@ -2,8 +2,8 @@ import json
 
 from django.contrib.auth.models import User
 from django.urls import reverse
-from library.library.models import Author, Book, Editorial
-from library.library.serializers import AuthorSerializer
+from library.api.models import Author, Book, Editorial
+from library.api.serializers import AuthorSerializer
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
